f3965c1fb5f551507218119618d59eb4bffb6388
doerr <chenjunbin1245@126.com>
2025-05-12 19:00:09 +0800

    完全使用moveto试试？

--- ActionModule.cpp
+++ ActionModule.cpp
@@ -106,13 +106,13 @@ void ActionModule::ProcessLoop() {
 
                 
 
-                // 归一化移动值到±10范围
-                auto normalizedMove = NormalizeMovement(centerToTargetX, centerToTargetY, 5.0f);
+                //// 归一化移动值到±10范围
+                //auto normalizedMove = NormalizeMovement(centerToTargetX, centerToTargetY, 5.0f);
 
                 // 使用KMBOX控制器移动鼠标(相对坐标)
-                mouseController->MoveRelative(
-                    static_cast<int>(normalizedMove.first),
-                    static_cast<int>(normalizedMove.second));
+                mouseController->MoveTo(
+                    static_cast<int>(centerToTargetX),
+                    static_cast<int>(centerToTargetY));
 
               
             }
